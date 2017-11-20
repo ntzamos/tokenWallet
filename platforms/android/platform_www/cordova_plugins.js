6711b3813fda1fe25ae6aa8f26727af8da1e9443
commando <commando@commandos-MBP.lan>
2017-11-20 15:09:38 +0200

    share address

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -15,6 +15,20 @@ module.exports = [
     "clobbers": [
       "cordova.plugins.barcodeScanner"
     ]
+  },
+  {
+    "id": "es6-promise-plugin.Promise",
+    "file": "plugins/es6-promise-plugin/www/promise.js",
+    "pluginId": "es6-promise-plugin",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-x-socialsharing.SocialSharing",
+    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+    "pluginId": "cordova-plugin-x-socialsharing",
+    "clobbers": [
+      "window.plugins.socialsharing"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -23,7 +37,9 @@ module.exports.metadata =
   "cordova-plugin-console": "1.0.7",
   "cordova-plugin-statusbar": "1.0.1",
   "cordova-plugin-whitelist": "1.2.2",
-  "phonegap-plugin-barcodescanner": "7.0.0"
+  "phonegap-plugin-barcodescanner": "7.0.0",
+  "es6-promise-plugin": "4.1.0",
+  "cordova-plugin-x-socialsharing": "5.2.1"
 };
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -56,6 +56,28 @@ function qrScan(){
    );
  }
 }
+
+$(".showaddress").click(function(){
+  var options = {
+    message: address, // not supported on some apps (Facebook, Instagram)
+    subject: 'My address', // fi. for email
+    files: ['', ''], // an array of filenames either locally or remotely
+    url: '',
+    chooserTitle: 'Share address' // Android only, you can override the default share sheet title
+  }
+
+  var onSuccess = function(result) {
+    console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+    console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+  }
+
+  var onError = function(msg) {
+    console.log("Sharing failed with message: " + msg);
+  }
+
+  window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+
+});
 // Handle Cordova Device Ready Event
 $(document).ready(function() {
     console.log("Device is ready!");
